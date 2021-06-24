4c116d7db34a07aec68e82ab8ab870ebf1697480
Catherine <cath.thedim@gmail.com>
2021-06-24 00:06:39 -0300

    Adição de testes da calculadora

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -44,4 +44,31 @@ public class CalculadoraTest {
         assertThrows(ArithmeticException.class,
                 () -> calc.divisao(8, 0));
     }
+
+    @Test
+    public void testSubtracaoDoisNumeros() {
+        int subtracao = calc.subtracao(10, 5);
+        assertEquals(5, subtracao);
+    }
+
+    @Test
+    public void testMultiplicacaoDoisNumeros() {
+        int multiplicacao = calc.multiplicacao(10, 7);
+        assertEquals(70, multiplicacao);
+    }
+
+    @Test
+    public void testSomatoria() {
+        int somatoria = calc.somatoria(3);
+        assertEquals(6, somatoria);
+    }
+
+    @Test
+    public void testNumeroEhPositivo() {
+        Boolean EhPositivo1 = calc.ehPositivo(-4);
+        assertEquals(false, EhPositivo1);
+
+        Boolean EhPositivo2 = calc.ehPositivo(6);
+        assertEquals(true, EhPositivo2);
+    }
 }
